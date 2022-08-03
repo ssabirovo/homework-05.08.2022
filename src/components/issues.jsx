e5f2fc01fd576c3452bae9f47f5d108524e31951
MuhammaddiyorQobulov <muhammaddiyor20032808@gmail.com>
2022-08-03 18:22:14 +0500

    compared raxmonbek codes (filter section)

--- src/components/issues.jsx
+++ src/components/issues.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 import { getIssues } from "./utils/get-issues";
 import Issue from "./issue";
+import FilterBtns from "./filterBtns";
 
 export class Issues extends Component {
+  state = {
+    finished: getIssues().filter((issue) => issue.isFinished === true),
+    done: getIssues().filter((issue) => issue.isFinished === false),
+    renderArray: getIssues(),
+    btnsArray: ["all", "finished", "onProgress"],
+  };
+
+  handeleFilter = (type) => {
+    if (type === "onProgress") {
+      this.setState({ renderArray: this.state.done });
+    }
+    if (type === "finished") {
+      this.setState({ renderArray: this.state.finished });
+    }
+    if (type === "all") {
+      this.setState({ renderArray: getIssues() });
+    }
+  };
+
   render() {
+    console.log(this.state);
     return (
       <div className="issues">
-        {getIssues(15).map((issue, idx) => (
+        <FilterBtns onfilter={this.handeleFilter} btns={this.state.btnsArray} />
+        {this.state.renderArray.map((issue, idx) => (
           <Issue issues={issue} key={issue.id} />
         ))}
       </div>
