954baece429c5e35de0900957faf6fd15eae2913
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-08-04 16:55:56 +0500

    add new component for toggle button

---
+++ src/components/toggle-zone/button.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./button.scss";
+
+export class Button extends Component {
+  render() {
+    return (
+      <div>
+        <button className="btns">sigir</button>
+        <h1 className="btns-title">Katta sigir</h1>
+      </div>
+    );
+  }
+}
+
+export default Button;
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Link from "../components/links/link";
-import Search from "../components/search/search";
+// import Search from "../components/search/search";
+import Button from "../components/toggle-zone/button";
 
 export class Dashboard extends Component {
   render() {
     return (
       <div>
         <Link />
+        <Button />
       </div>
     );
   }
